d18f66ee88a84a300b76bc7e02799015c8161aa9
Wong Wei Han <56218253+WeiHanWong@users.noreply.github.com>
2024-09-28 04:57:55 +0800

    Add remark command

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.AddressBookParser;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+
+public class RemarkCommandTest {
+
+//    String MESSAGE_NOT_IMPLEMENTED_YET = "NOT IMPLEMENTED YET";
+//    private final AddressBookParser parser = new AddressBookParser();
+//    private Model model;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+//    }
+//
+//    @Test
+//    public void execute() {
+//        assertCommandFailure(new RemarkCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+//    }
+//
+//    @Test
+//    public void parseCommand_remark() throws Exception {
+//        assertTrue(parser.parseCommand(RemarkCommand.COMMAND_WORD) instanceof RemarkCommand);
+//    }
+}
